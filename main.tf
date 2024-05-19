9ef96ca01eb3162bdcd1960fb73ea840e10edd17
Alcimovich <alcimovichnoubissi@gmail.com>
2024-05-19 13:43:25 +0100

    update variable data structure

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-locals {
-  secret_list = flatten([for project, value in var.adapter_secret :
-    flatten([for index, secret in value :
-      {
-        project = project
-        secret  = lower(secret)
-      }
-    ])
-  ])
-}
-
 resource "google_secret_manager_secret" "adapter_secret" {
-  for_each = tomap({ for index, record in local.secret_list : index => record })
+  for_each = var.adapter_secret
 
   provider  = google.secure
-  secret_id = "prod-app-base-060623-${each.value.project}-${each.value.secret}-${terraform.workspace}"
+  secret_id = "prod-app-base-060623-${each.key}-${each.value}-${terraform.workspace}"
 
   replication {
     auto {}
