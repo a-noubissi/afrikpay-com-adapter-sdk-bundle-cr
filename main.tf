e239ee7c949112500fea3e53e54154d350ca92c6
Alcimovich <alcimovichnoubissi@gmail.com>
2024-05-21 09:23:35 +0100

    update data structure

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
 resource "google_secret_manager_secret" "adapter_secret" {
-  for_each = toset(var.adapter_secret)
+  count = length(var.adapter_secret)
 
   provider  = google.secure
-  secret_id = "prod-app-base-060623-${each.value}-${terraform.workspace}"
+  secret_id = "prod-app-base-060623-${var.adapter_secret[count.index]}-${terraform.workspace}"
 
   replication {
     auto {}
